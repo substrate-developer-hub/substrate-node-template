bda07b4c3c21de9358a37f91dba74884756b9be3
Shawn Tabrizi <shawntabrizi@gmail.com>
2021-01-17 00:51:51 -0400

    Update runtime/src/lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -94,9 +94,9 @@ pub mod opaque {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node-template"),
 	impl_name: create_runtime_str!("node-template"),
-	authoring_version: 100,
+	authoring_version: 1,
 	spec_version: 100,
-	impl_version: 100,
+	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 100,
 };
