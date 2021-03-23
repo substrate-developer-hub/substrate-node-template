67d81e6d309127fdd39b9d3903b0ee4d637e6e9d
Luke Schoen <ltfschoen@gmail.com>
2021-03-23 19:21:03 +0100

    add pseudocode

--- pallets/calculator/src/lib.rs
+++ pallets/calculator/src/lib.rs
@@ -20,7 +20,25 @@ decl_storage! {
 		Something get(fn something): Option<u32>;
 
 		/// Substrate-fixed accumulator, value starts at 1 (multiplicative identity)
-        FixedAccumulator get(fn fixed_value): U16F16 = U16F16::from_num(1);
+		FixedAccumulator get(fn fixed_value): U16F16 = U16F16::from_num(1);
+
+		pub FixedRewardsPerDay get(fn rewards_daily):
+			map hasher(opaque_blake2_256) T::Moment =>
+				Option<Vec<(
+					<T as frame_system::Trait>::AccountId,
+					BalanceOf<T>,
+					<T as frame_system::Trait>::BlockNumber
+				>>;
+
+		/// Returns daily reward distribution block number corresponding to a given date/time
+		pub BlockRewardedForDay get(fn block_rewarded_for_day):
+			map hasher(opaque_blake2_256) T::Moment =>
+				Option<<T as frame_system::Trait>::BlockNumber>;
+
+		/// Returns date/time corresponding to a given daily reward distribution block number
+		pub DayRewardedForBlock get(fn day_rewarded_for_block):
+			map hasher(opaque_blake2_256) <T as frame_system::Trait>::BlockNumber =>
+				Option<T::Moment>;
 	}
 }
 
@@ -48,20 +66,47 @@ decl_module! {
 		fn deposit_event() = default;
 
 		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		fn update_fixed(origin, new_factor: U16F16) -> DispatchResult {
+		fn update_fixed_daily_rewards(origin, new_factor: U16F16) -> DispatchResult {
 			ensure_signed(origin)?;
 
-			let old_accumulated = Self::fixed_value();
+			let milliseconds_per_day = 86400000u64;
 
-			// Multiply, handling overflow
-			let new_product = old_accumulated.checked_mul(new_factor)
-				.ok_or(Error::<T>::Overflow)?;
+			// get the current block
 
-			// Write the new value to storage
-			FixedAccumulator::put(new_product);
+			// get the current date/time
+			let timestamp_current = <pallet_timestamp::Module<T>>::get();
 
-			// Emit event
-			Self::deposit_event(RawEvent::FixedUpdated(new_factor, new_product));
+			// convert the curreht block number to the block number at the start of the current day.
+
+			// convert the current date/time to the start of the current day date/time. i.e. 21 Apr @ 1420 -> 21 Apr @ 0000
+
+			// check if the start of the current day date/time entry exists as a key for `rewards_daily`
+			//
+			// if so, retrieve the latest `rewards_daily` data stored for the start of that day date/time
+			// i.e. (account_id, balance_rewarded, block_number), and add the new reward value to it.
+			//
+			// else just insert that as a new entry
+			//
+			// also, check if the start of the current day date/time entry exists as a key for `block_rewarded_for_day`,
+			// otherwise add it, with the block number corresponding to the start of the current day as the value
+			//
+			// repeat for `day_rewarded_for_block`
+			//
+			// lastly, update in storage the total rewards distributed so far for the current day
+			// so users may query state to get the calculated total.
+
+
+			// let old_accumulated = Self::fixed_value();
+
+			// // Multiply, handling overflow
+			// let new_product = old_accumulated.checked_mul(new_factor)
+			// 	.ok_or(Error::<T>::Overflow)?;
+
+			// // Write the new value to storage
+			// FixedAccumulator::put(new_product);
+
+			// // Emit event
+			// Self::deposit_event(RawEvent::FixedUpdated(new_factor, new_product));
 			Ok(())
 		}
 
