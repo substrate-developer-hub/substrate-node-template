e6604b810f40453fd6f55bf0e3a7a009546c04b4
Jimmy Chu <jimmychu0807@gmail.com>
2021-09-02 12:52:54 +0800

    Updated

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -42,6 +42,7 @@ pub use sp_runtime::{Perbill, Permill};
 
 /// Import the template pallet.
 pub use pallet_template;
+pub use pallet_kitties;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -270,6 +271,12 @@ impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
 
+/// Configure the pallet-kitties in pallets/kitties.
+impl pallet_template::Config for Runtime {
+	type Event = Event;
+	type KittyRandomness = RandomnessCollectiveFlip;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -287,6 +294,7 @@ construct_runtime!(
 		Sudo: pallet_sudo::{Pallet, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template::{Pallet, Call, Storage, Event<T>},
+		SubstrateKitties: pallet_kitties::{Module, Call, Config<T>, Storage, Event<T>},
 	}
 );
 
