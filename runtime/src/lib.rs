c0d7b119348a4b7a81161d89b6ca3879288e8ba1
cpath <cpathlu@gmail.com>
2021-12-19 17:52:25 +0800

    s1

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -279,6 +279,16 @@ impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
 
+parameter_types! {
+	pub const MaxClaimLength: u32 = 256;
+}
+
+/// Configure the pallet-poe in pallets/poe.
+impl pallet_poe::Config for Runtime {
+	type Event = Event;
+	type MaxClaimLength = MaxClaimLength;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -296,6 +306,8 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		// Include the custom logic from the pallet-poe in the runtime.
+		PoeModule: pallet_poe,
 	}
 );
 
