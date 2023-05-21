803c606ac21ce9cd8b3182e21d85e4d794095293
root <root@testnet-validator1.aisland.io>
2023-05-21 04:57:41 +0200

    removed authorisation pallet

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -8,8 +8,6 @@ use sp_consensus_grandpa::AuthorityId as GrandpaId;
 use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
 use sp_core::crypto::UncheckedInto;
-use sp_core::OpaquePeerId; // A struct wraps Vec<u8> to represent the node `PeerId`.
-use aisland_runtime::NodeAuthorizationConfig; // The genesis config that serves the pallet.
 use hex_literal::hex;
 // The URL for the telemetry server.
 // const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -235,22 +233,6 @@ fn testnet_genesis(
 			key: Some(root_key),
 		},
 		transaction_payment: Default::default(),
-		node_authorization: NodeAuthorizationConfig {
-			nodes: vec![
-			  (
-				OpaquePeerId(bs58::decode("12D3KooWSaStSZDjwJqp75uqf5gzvFXzRVsHmELmmUNYBky6u2em").into_vec().unwrap()),
-				endowed_accounts[0].clone()
-			  ),
-			  (
-				OpaquePeerId(bs58::decode("12D3KooWMpXRsmX1JU1wwtnMHLQ22ai53G4uTDGHr2ZF3tMv6LHq").into_vec().unwrap()),
-				endowed_accounts[1].clone()
-			  ),
-			  (
-				OpaquePeerId(bs58::decode("12D3KooWSHdwqGgQKBGdRcZeHWdmMwxNvHnhuAovpYgih4VYBgJA").into_vec().unwrap()),
-				endowed_accounts[1].clone()
-			  ),
-			],
-		  },
 		
 	}
 }
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -23,7 +23,6 @@ use sp_std::prelude::*;
 use sp_version::NativeVersion;
 use sp_version::RuntimeVersion;
 
-use frame_system::EnsureRoot;
 
 // A few exports that help ease life for downstream crates.
 pub use frame_support::{
@@ -276,21 +275,6 @@ impl pallet_marketplace::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = pallet_marketplace::weights::SubstrateWeight<Runtime>;
 }
-parameter_types! {
-	pub const MaxWellKnownNodes: u32 = 128;
-	pub const MaxPeerIdLength: u32 = 128;
-   }
-impl pallet_node_authorization::Config for Runtime {
-	type RuntimeEvent = RuntimeEvent;
-	type MaxWellKnownNodes = MaxWellKnownNodes;
-	type MaxPeerIdLength = MaxPeerIdLength;
-	type AddOrigin = EnsureRoot<AccountId>;
-	type RemoveOrigin = EnsureRoot<AccountId>;
-	type SwapOrigin = EnsureRoot<AccountId>;
-	type ResetOrigin = EnsureRoot<AccountId>;
-	type WeightInfo = ();
-   }
-
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
@@ -309,7 +293,6 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic
 		MarketPlace: pallet_marketplace,
-		NodeAuthorization: pallet_node_authorization::{Pallet, Call, Storage, Event<T>, Config<T>},
 		
 	}
 );
