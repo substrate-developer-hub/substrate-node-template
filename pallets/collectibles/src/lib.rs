10a225e8c379e87f64e16444639dbdb238d29d57
KavinduShamalka <kavindusuraweera27@gmail.com>
2023-09-17 09:51:00 -0700

    add collectibles pallet

---
+++ pallets/collectibles/src/lib.rs
@@ -0,0 +1,24 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+
+#[frame_support::pallet(dev_mode)]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+    use frame_support::traits::{Currency, Randomness};
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type Currency: Currency<Self::AccountId>;
+        type CollectionRandomness: Randomness<Self::Hash, Self::BlockNumber>;
+    
+        #[pallet::constant]
+        type MaximumOwned: Get<u32>;
+    }
+}
