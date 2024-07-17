3fd95c304dda7620afc2f3864a00db1bbc1cf2a4
nwizugbesamson <samson6398@gmail.com>
2024-07-17 13:29:07 +0100

    Supply chain management Logic

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -41,8 +41,8 @@ use pallet_transaction_payment::{ConstFeeMultiplier, CurrencyAdapter, Multiplier
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
-/// Import the template pallet.
-pub use pallet_template;
+/// Import the supply chain management pallet.
+pub use pallet_supply_chain_management;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -245,10 +245,10 @@ impl pallet_sudo::Config for Runtime {
 	type WeightInfo = pallet_sudo::weights::SubstrateWeight<Runtime>;
 }
 
-/// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+/// Configure the pallet-supply-chain-management in pallets/supply_chain_management.
+impl pallet_supply_chain_management::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
-	type WeightInfo = pallet_template::weights::SubstrateWeight<Runtime>;
+	type WeightInfo = pallet_supply_chain_management::weights::SubstrateWeight<Runtime>;
 }
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
@@ -289,9 +289,9 @@ mod runtime {
 	#[runtime::pallet_index(6)]
 	pub type Sudo = pallet_sudo;
 
-	// Include the custom logic from the pallet-template in the runtime.
+	// Include the custom logic from the pallet-supply-chain-management in the runtime.
 	#[runtime::pallet_index(7)]
-	pub type TemplateModule = pallet_template;
+	pub type TemplateModule = pallet_supply_chain_management;
 }
 
 /// The address format for describing accounts.
@@ -341,7 +341,7 @@ mod benches {
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
 		[pallet_sudo, Sudo]
-		[pallet_template, TemplateModule]
+		[pallet_supply_chain_management, TemplateModule]
 	);
 }
 
